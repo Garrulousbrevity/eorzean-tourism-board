3f0e07ae8f121fd7d3fc557b31ae8648e35b259a
Sean Ezrol <garrulousbrevity@gmail.com>
2023-02-17 17:19:43 -0500

    Add new files to do basic window finding, and a first draft of how to display that

---
+++ src/Constants.js
@@ -0,0 +1,201 @@
+import EorzeaWeather from 'eorzea-weather';
+
+
+export const DATA = [
+  // {
+  //   "Key" : 0,
+  //   "Name": "",
+  //   "ZoneId": "",
+  //   "ZoneName": null,
+  //   "Coordinates": {
+  //     "X": null,
+  //     "Y": null
+  //   },
+  //   "Weather": [
+
+  //   ],
+  //   "Window": {
+  //     "StartTime": 8,
+  //     "EndTime": 12
+  //   },
+  //   "EndTime": null,
+  //   "Emote": "",
+  //   "Comment": ""
+  // },
+  {
+    "Key": 1,
+    "Name": "Barracuda Piers",
+    "ZoneId": EorzeaWeather.ZONE_LIMSA_LOMINSA,
+    "ZoneName": "Limsa Lominsa Upper Decks",
+    "Coordinates": {
+      "X": 9.5,
+      "Y": 7.8
+    },
+    "Weather": [
+      "Fair Skies",
+      "Clear Skies"
+    ],
+    "Window": {
+      "StartTime": 8,
+      "EndTime": 12
+    },
+    "Emote": "Lookout",
+    "Comment": "Located northwest out of The Bismarck, there are two sets of bricks set into the wall there. The set of bricks closer to the stairs is the Vista location.",
+    "URL": "https://ffxiv.consolegameswiki.com/wiki/Sightseeing_Log_1:_Barracuda_Piers"
+  },
+  {
+    "Key": 1,
+    "Name": "Barracuda Piers",
+    "ZoneId": EorzeaWeather.ZONE_LIMSA_LOMINSA,
+    "ZoneName": "Limsa Lominsa Upper Decks",
+    "Coordinates": {
+      "X": 9.5,
+      "Y": 7.8
+    },
+    "Weather": [
+      "Fair Skies",
+      "Clear Skies"
+    ],
+    "Window": {
+      "StartTime": 8,
+      "EndTime": 12
+    },
+    "Emote": "Lookout",
+    "Comment": "Located northwest out of The Bismarck, there are two sets of bricks set into the wall there. The set of bricks closer to the stairs is the Vista location.",
+    "URL": "https://ffxiv.consolegameswiki.com/wiki/Sightseeing_Log_1:_Barracuda_Piers"
+  },
+  {
+    "Key": 1,
+    "Name": "Barracuda Piers",
+    "ZoneId": EorzeaWeather.ZONE_LIMSA_LOMINSA,
+    "ZoneName": "Limsa Lominsa Upper Decks",
+    "Coordinates": {
+      "X": 9.5,
+      "Y": 7.8
+    },
+    "Weather": [
+      "Fair Skies",
+      "Clear Skies"
+    ],
+    "Window": {
+      "StartTime": 8,
+      "EndTime": 12
+    },
+    "Emote": "Lookout",
+    "Comment": "Located northwest out of The Bismarck, there are two sets of bricks set into the wall there. The set of bricks closer to the stairs is the Vista location.",
+    "URL": "https://ffxiv.consolegameswiki.com/wiki/Sightseeing_Log_1:_Barracuda_Piers"
+  },
+  {
+    "Key": 1,
+    "Name": "Barracuda Piers",
+    "ZoneId": EorzeaWeather.ZONE_LIMSA_LOMINSA,
+    "ZoneName": "Limsa Lominsa Upper Decks",
+    "Coordinates": {
+      "X": 9.5,
+      "Y": 7.8
+    },
+    "Weather": [
+      "Fair Skies",
+      "Clear Skies"
+    ],
+    "Window": {
+      "StartTime": 8,
+      "EndTime": 12
+    },
+    "Emote": "Lookout",
+    "Comment": "Located northwest out of The Bismarck, there are two sets of bricks set into the wall there. The set of bricks closer to the stairs is the Vista location.",
+    "URL": "https://ffxiv.consolegameswiki.com/wiki/Sightseeing_Log_1:_Barracuda_Piers"
+  },
+  {
+    "Key": 1,
+    "Name": "Barracuda Piers",
+    "ZoneId": EorzeaWeather.ZONE_LIMSA_LOMINSA,
+    "ZoneName": "Limsa Lominsa Upper Decks",
+    "Coordinates": {
+      "X": 9.5,
+      "Y": 7.8
+    },
+    "Weather": [
+      "Fair Skies",
+      "Clear Skies"
+    ],
+    "Window": {
+      "StartTime": 8,
+      "EndTime": 12
+    },
+    "Emote": "Lookout",
+    "Comment": "Located northwest out of The Bismarck, there are two sets of bricks set into the wall there. The set of bricks closer to the stairs is the Vista location.",
+    "URL": "https://ffxiv.consolegameswiki.com/wiki/Sightseeing_Log_1:_Barracuda_Piers"
+  },
+  {
+    "Key": 1,
+    "Name": "Barracuda Piers",
+    "ZoneId": EorzeaWeather.ZONE_LIMSA_LOMINSA,
+    "ZoneName": "Limsa Lominsa Upper Decks",
+    "Coordinates": {
+      "X": 9.5,
+      "Y": 7.8
+    },
+    "Weather": [
+      "Fair Skies",
+      "Clear Skies"
+    ],
+    "Window": {
+      "StartTime": 8,
+      "EndTime": 12
+    },
+    "Emote": "Lookout",
+    "Comment": "Located northwest out of The Bismarck, there are two sets of bricks set into the wall there. The set of bricks closer to the stairs is the Vista location.",
+    "URL": "https://ffxiv.consolegameswiki.com/wiki/Sightseeing_Log_1:_Barracuda_Piers"
+  },
+  {
+    "Key": 1,
+    "Name": "Barracuda Piers",
+    "ZoneId": EorzeaWeather.ZONE_LIMSA_LOMINSA,
+    "ZoneName": "Limsa Lominsa Upper Decks",
+    "Coordinates": {
+      "X": 9.5,
+      "Y": 7.8
+    },
+    "Weather": [
+      "Fair Skies",
+      "Clear Skies"
+    ],
+    "Window": {
+      "StartTime": 8,
+      "EndTime": 12
+    },
+    "Emote": "Lookout",
+    "Comment": "Located northwest out of The Bismarck, there are two sets of bricks set into the wall there. The set of bricks closer to the stairs is the Vista location.",
+    "URL": "https://ffxiv.consolegameswiki.com/wiki/Sightseeing_Log_1:_Barracuda_Piers"
+  },
+];
+
+export const WEATHER_MAPPING = {
+  "Fair Skies": "Fair_Skies.png",
+  "Clear Skies": "Clear_Skies.png"
+};
+
+export const EMOTE_MAPPING = {
+  "Lookout": "Lookout.png"
+};
+
+export const PHASES = [
+  {
+    StartTime: 0,
+    EndTime: 7
+  },
+  {
+    StartTime: 8,
+    EndTime: 15
+  },
+  {
+    StartTime: 16,
+    EndTime: 23
+  }
+];
+
+export const ONE_HOUR = 175 * 1000;
+export const EIGHT_HOURS = ONE_HOUR * 8;
+export const ONE_DAY = EIGHT_HOURS * 3;
+export const DAYS_TO_CHECK = 10
