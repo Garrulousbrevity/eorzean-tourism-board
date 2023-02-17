742d39e6f957bda520a2731342236f1a4784ef48
Sean Ezrol <sean.ezrol@ellevationeducation.com>
2023-02-17 17:19:43 -0500

    Add new files to do basic window finding, and a first draft of how to display that

---
+++ src/SightLog.js
@@ -0,0 +1,78 @@
+import EorzeaWeather from 'eorzea-weather';
+import EorzeaTime from 'eorzea-time';
+import { EIGHT_HOURS, ONE_HOUR, PHASES } from './Constants';
+import { useState, useEffect } from 'react';
+import SightLogView from './SightLogView';
+
+const Times_Intersect = (Range1, Range2) => {
+  if (Range1.StartTime > Range1.EndTime) {
+    return Times_Intersect(Range2, {StartTime: Range1.EndTime, EndTime: 24}) || Times_Intersect(Range2, {StartTime: 0, EndTime: Range1.StartTime});
+  }
+  if (Range2.StartTime > Range2.EndTime) {
+    return Times_Intersect(Range1, {StartTime: Range2.EndTime, EndTime: 24}) || Times_Intersect(Range1, {StartTime: 0, EndTime: Range2.StartTime});
+  }
+  return Range1.StartTime <= Range2.EndTime && Range1.EndTime >= Range2.StartTime;
+};
+
+const getTimeDisplay = time => {
+  if (time === 0) return '12AM';
+  if (time === 12) return '12PM';
+  if (time > 11) return `${time % 12}PM`;
+  return `${time}AM`; 
+}
+
+function SightLog({
+  log,
+  times,
+  onRefresh
+}) {
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
+  const [windowStartDisplay, setWindowStartDisplay] = useState('');
+  const [windowEndDisplay, setWindowEndDisplay] = useState('');
+
+  useEffect(() => {
+    setStartTime(null);
+    setEndTime(null);
+    const eWeather = new EorzeaWeather(log.ZoneId);
+    const phases = PHASES.filter(phase => Times_Intersect(phase, log.Window));
+    const goodWeatherTime = times.find(time => {
+      if (new Date(time + EIGHT_HOURS) < new Date()) return false;
+      const date = new Date(time);
+      if (!phases.some(phase => phase.StartTime === new EorzeaTime(date).getHours())) return false;
+      const actualWeather = eWeather.getWeather(date)
+      return log.Weather.some(allowedWeather => allowedWeather === actualWeather)
+    });
+    if (goodWeatherTime != null) {
+      const phase = phases.find(phase => phase.StartTime === new EorzeaTime(new Date(goodWeatherTime)).getHours());
+      const startOffset = Math.max(log.Window.StartTime, phase.StartTime) - phase.StartTime;
+      setStartTime(new Date(goodWeatherTime + (ONE_HOUR * startOffset)));
+      let endOffset = log.Window.EndTime - (log.Window.StartTime + startOffset);
+      if ((log.Window.StartTime > log.Window.EndTime || log.Window.EndTime > phase.EndTime) && log.Weather.some(weather => weather === eWeather.getWeather(new Date(goodWeatherTime + EIGHT_HOURS)))) {
+        if (log.Window.StartTime > log.Window.EndTime) {
+          endOffset += log.Window.StartTime;
+        } else {
+          endOffset += log.Window.EndTime - phase.EndTime;
+        }
+      }
+      setEndTime(new Date(goodWeatherTime + (endOffset * ONE_HOUR)));
+    }
+  }, [log.Weather, log.Window, log.ZoneId, times])
+
+  useEffect(() => {
+    setWindowStartDisplay(getTimeDisplay(log.Window.StartTime));
+    setWindowEndDisplay(getTimeDisplay(log.Window.EndTime));
+  }, [log.Window.StartTime, log.Window.EndTime]);
+
+  return (
+    <SightLogView 
+      {...log}
+      CollectableWindowStartTime={startTime}
+      CollectableWindowEndTime={endTime}
+      WindowStartDisplay={windowStartDisplay}
+      WindowEndDisplay={windowEndDisplay}
+    />
+  );
+};
+
+export default SightLog
---
+++ src/SightLogView.js
@@ -0,0 +1,70 @@
+import { DAYS_TO_CHECK, WEATHER_MAPPING, EMOTE_MAPPING } from "./Constants";
+import { Avatar, Card, CardContent, CardHeader, Chip, Link, Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+
+function SightLogView({
+  Key,
+  Name,
+  ZoneName,
+  Coordinates,
+  Weather,
+  Window,
+  Emote,
+  Comment,
+  URL,
+  CollectableWindowStartTime,
+  CollectableWindowEndTime,
+  WindowStartDisplay,
+  WindowEndDisplay
+}) {
+  return (
+    <Card>
+      <CardHeader
+        title={
+          <Link href={URL} underline="hover">
+            {Key}. {Name}
+          </Link>
+        }
+        subheader={`${ZoneName} (${Coordinates.X}, ${Coordinates.Y})`}
+      />
+      <CardContent>
+        <Grid container spacing={1}>
+          <Grid xs={4}>
+            <Chip
+              avatar={<Avatar alt={Emote} src={`emotes/${EMOTE_MAPPING[Emote]}`} />}
+              label={Emote}                       
+            />
+          </Grid>
+          <Grid xs={4}>
+            <Chip
+              label={`${WindowStartDisplay} - ${WindowEndDisplay}`}
+            />
+          </Grid>
+          <Grid xs={4} container>
+            {Weather.map(condition => (
+              <Grid xs={12}>
+                <Chip size="small"
+                  avatar={<Avatar alt={condition} src={`weather/${WEATHER_MAPPING[condition]}`} />}
+                  label={condition}
+                />
+              </Grid>
+            ))}
+          </Grid>
+          {CollectableWindowStartTime == null || CollectableWindowEndTime == null ? (
+          <Typography variant="h4">N/A</Typography> 
+            ) : (
+                <Grid xs={12}>
+                  <Typography variant="h6">Available: {CollectableWindowStartTime.toLocaleTimeString()} - {CollectableWindowEndTime.toLocaleTimeString()}</Typography>
+                </Grid>
+            )}
+            <Grid xs={12}>
+              <Typography paragraph>{Comment}</Typography>
+            </Grid>
+        </Grid>
+
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SightLogView
