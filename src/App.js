f6298038e8c976f9f047860a8f06b82ae716e324
Sean Ezrol <garrulousbrevity@gmail.com>
2023-02-17 17:18:16 -0500

    Creates skeleton of app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useMemo } from 'react';
+import {DATA, ONE_HOUR, EIGHT_HOURS, ONE_DAY, DAYS_TO_CHECK} from './Constants';
+import range from 'lodash/range'
+import SightLog from './SightLog';
+import { Container } from '@mui/system';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Grid from '@mui/material/Unstable_Grid2';
+
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [times, setTimes] = useState([]);
+  const [lastRefresh, setLastRefresh] = useState(new Date());
+  const [logs, setLogs] = useState(DATA);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
+  useEffect(() => {
+    const msec = new Date().getTime();
+    const bell = (msec / ONE_HOUR) % 24;
+    const startMsec = msec - Math.round(ONE_HOUR * bell);
+    setTimes(range(
+      startMsec,
+      startMsec + ONE_DAY * DAYS_TO_CHECK,
+      EIGHT_HOURS,
+    ));
+  }, [
+    lastRefresh
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container component="main">
+        <Grid container spacing={2}>
+          {logs.map((log) => (
+          <Grid xs={4} key={log.Key}>
+            <SightLog
+              log={log}
+              times={times}
+              onRefresh={() => setLastRefresh(new Date())}
+            />
+          </Grid>
+        ))}
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
