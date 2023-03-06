8c61e2e5ff05f629435462d8a4f53bd2c219c36f
Jenn Collins <jennifer.nagle423@gmail.com>
2023-03-06 13:13:50 -0500

    Add test and fix stupid import I hate coding sometimes

--- src/SightLogView.js
+++ src/SightLogView.js
@@ -11,7 +11,7 @@ import {
   Link,
   Typography,
 } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2/Grid2';
+import Grid from '@mui/material/Unstable_Grid2';
 import { CheckCircle, CheckCircleOutlineOutlined } from '@mui/icons-material';
 
 function SightLogView({
